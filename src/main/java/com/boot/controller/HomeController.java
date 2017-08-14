b284aa7d3f963f0a45b3512f9f388015ee7d3266
Dhruv Kalaria <dhruvkalaria@gmail.com>
2017-08-13 21:35:51 -0700

    served html from controller

--- src/main/java/com/boot/controller/HomeController.java
+++ src/main/java/com/boot/controller/HomeController.java
@@ -1,16 +1,17 @@
 package com.boot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by dhruvkalaria on 8/13/17.
  */
-@RestController
+@Controller
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
-        return "Das-Boot";
+        return "index";
     }
 }
